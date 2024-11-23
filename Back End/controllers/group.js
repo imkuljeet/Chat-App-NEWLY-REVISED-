a3aa9561268ad8522e661af2df43eca1ce6f1f04
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-11-24 00:59:08 +0530

    UserGroup table is updated when a group is created.

--- Back End/controllers/group.js
+++ Back End/controllers/group.js
@@ -1,11 +1,18 @@
 const Group = require('../models/groups');
+const UserGroup = require('../models/usergroup');
 
 const namegroup = async (req, res, next) => {
     try {
+        console.log(req.body);
+
         const { groupName } = req.body;
+        const userId = req.user.id; // Assuming req.user.id is set and contains the user ID
 
         // Create a new group
         const group = await Group.create({ groupName });
+
+        // Update UserGroup table with the new group and user ID
+        await UserGroup.create({ userId: userId, groupId: group.id });
 
         // Send a success response
         res.status(201).json({
