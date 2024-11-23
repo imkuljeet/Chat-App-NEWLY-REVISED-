351f2852cb7ddb57ce96338ac14e52878f0374c6
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-11-24 00:08:34 +0530

    Encrypt Password using Bcrypt

--- Back End/controllers/user.js
+++ Back End/controllers/user.js
@@ -1,16 +1,25 @@
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
 const signup = async (req, res, next) => {
     try {
-          const { name, email, phone, password } = req.body;
+        const { name, email, phone, password } = req.body;
 
         // Check if the user already exists
         const existingUser = await User.findOne({ where: { email } });
         if (existingUser) {
-            return res.status(409).json({ success: false, message: 'User already Exists, Please Login' });
+            return res.status(409).json({ success: false, message: 'User already exists, please login.' });
         }
 
-        const userCreated = await User.create({ name, email, phone, password });
+        const saltrounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltrounds);
+        
+        const userCreated = await User.create({ 
+            name, 
+            email, 
+            phone, 
+            password: hashedPassword 
+        });
 
         res.status(201).json({ success: true, user: userCreated });
     } catch (err) {
