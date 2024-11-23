181d20198e42ba1584277fd4f258b8e7935f8edd
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-11-24 00:07:34 +0530

    User already exists,please login message alert

--- Back End/controllers/user.js
+++ Back End/controllers/user.js
@@ -1,9 +1,22 @@
 const User = require('../models/users');
 
-const signup = (req, res, next) => {
-    console.log(req.body);
+const signup = async (req, res, next) => {
+    try {
+          const { name, email, phone, password } = req.body;
 
-    
-}
+        // Check if the user already exists
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(409).json({ success: false, message: 'User already Exists, Please Login' });
+        }
+
+        const userCreated = await User.create({ name, email, phone, password });
+
+        res.status(201).json({ success: true, user: userCreated });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false, error: 'Internal Server Error' });
+    }
+};
 
 module.exports = { signup };
--- Front End/signup.js
+++ Front End/signup.js
@@ -1,27 +1,41 @@
 async function signup(e) {
-    try {
-        e.preventDefault();
+  try {
+    e.preventDefault();
 
-        let name = e.target.name.value;
-        let email = e.target.email.value;
-        let phone = e.target.phone.value;
-        let password = e.target.password.value;
+    let name = e.target.name.value;
+    let email = e.target.email.value;
+    let phone = e.target.phone.value;
+    let password = e.target.password.value;
 
-        let userDetails = {
-            name,
-            email,
-            phone,
-            password,
-        };
+    let userDetails = {
+      name,
+      email,
+      phone,
+      password,
+    };
 
-        // console.log(userDetails);
-        let response = await axios.post("http://localhost:3000/user/signup",userDetails);
-    } catch (err) {
-        console.log(err);
-        showError(err);
+    // console.log(userDetails);
+    let response = await axios.post("http://localhost:3000/user/signup",userDetails);
+
+    console.log("RESPONSE>>>>>>", response);
+
+    if (response.status == 201) {
+      alert("Successfully Signed Up");
+    } else {
+      alert(response.data.message);
     }
+  } catch (err) {
+    if (err.response) {
+      console.log("Error Response:", err.response);
+      alert(`${err.response.data.message || "Something went wrong"}`);
+    } else {
+      console.log("Error:", err.message);
+      alert("An unknown error occurred");
+    }
+    showError(err);
+  }
 }
 
 function showError(err) {
-    document.body.innerHTML += `Error is - <span style="color: red;">${err}</span>`;
+  document.body.innerHTML += `Error is - <span style="color: red;">${err}</span>`;
 }
